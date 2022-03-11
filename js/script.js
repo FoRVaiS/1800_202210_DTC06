9c39e151f047f84fb661df7a32d9d8ec36537731
Benjamin Chiang <chiangbenjamin991@gmail.com>
2022-03-10 20:44:15 -0800

    feat: load user data into localStorage

--- js/script.js
+++ js/script.js
@@ -3,6 +3,40 @@ function emulateSignIn(user = "Test User") {
     $('.navbar__profile').attr("data-signed-in", true);
 }
 
+/** Query the database to fetch the user document */
+function fetchUserDoc(uid) {
+    return db.collection("users").doc(uid).get();
+}
+
+/** Stores public user data in LocalStorage */
+function storeUserData(doc) {
+    const { name } = doc.data();
+
+    window.localStorage.setItem('username', name);
+}
+
+/** When the user successfully logs in, execute any post-login tasks */
+async function handleUserSession(user) {
+    const isUserLoggedIn = !!user;
+
+    if (isUserLoggedIn) {
+        if (!window.localStorage.getItem('username')) storeUserData(await fetchUserDoc(user.uid));
+
+        const username = window.localStorage.getItem('username');
+
+        $('#username').text(username);
+        $('.navbar__profile').attr('data-signed-in', true);
+    }
+}
+
+(() => {
+    const auth = firebase.auth();
+
+    auth.onAuthStateChanged(user => {
+        handleUserSession(user);
+    });
+})();
+
 function populateCardsDynamically() {
     let hikeCardTemplate = document.getElementById("hikeCardTemplate");
     let hikeCardGroup = document.getElementById("hikeCardGroup");
