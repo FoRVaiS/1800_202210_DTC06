6eb1eee9cadcd51f32247a81630d9f8e0f2e67dc
Benjamin Chiang <chiangbenjamin991@gmail.com>
2022-03-23 22:25:17 -0700

    feat: populate firestore user preferences

---
+++ pages/user_selection/user_selection.js
@@ -0,0 +1,29 @@
+(() => {
+    const validateMandatoryData = (form, onFailure) => {
+
+    }
+    const formRef = document.querySelector("form#user-settings");
+    formRef.addEventListener('submit', processForm);
+
+    function processForm(e) {
+        e.preventDefault();
+        const formData = Object.fromEntries($(formRef).serializeArray().map(Object.values));
+        const { activity_type: activityType, person_type: personType, bio } = formData;
+        // debugger;
+
+        firebase.auth().onAuthStateChanged(user => {
+            if (!user) return console.warn('User is not signed in!');
+
+            db.collection('users').doc(user.uid).update({
+                type: personType,
+                activity_type: activityType,
+                bio
+            }).then(() => {
+                window.location.href = "../main/main.html"
+            });
+        });
+
+
+        return false;
+    }
+})();
