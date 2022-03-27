5187aa0e84a57bf8c9ff3c876e5df54c40bc7369
Andrew Anca <andrewanca1998@hotmail.com>
2022-03-27 15:36:15 -0700

    created friends/chat window page

--- pages/page_info_template/page_info_template.js
+++ pages/page_info_template/page_info_template.js
@@ -47,3 +47,30 @@
         }
     });
 })();
+
+(() => {
+    const saveBtnRef = document.querySelector('#saveIcon');
+    const suggestionCode = saveBtnRef.getAttribute('data-id');
+
+    saveBtnRef.onclick = () => {
+        if (!suggestionCode) return console.warn("This button does not contain a data-id code");
+
+        firebase.auth().onAuthStateChanged(async(user) => {
+            if (user) { // This should only work if the user is signed in
+                const userDoc = db.collection("users").doc(user.uid);
+
+                try {
+                    // Add TYPE_ID to user's favourites
+                    const result = await userDoc.update({
+                        favourites: firebase.firestore.FieldValue.arrayUnion(suggestionCode)
+                    });
+                    saveBtnRef.setAttribute('data-id', param.code);
+                    // Success
+                } catch (e) {
+                    // Failure
+                    // Error stored in 'e'
+                }
+            }
+        });
+    }
+})();
