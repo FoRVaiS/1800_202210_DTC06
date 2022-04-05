4850805543f3d4baf6e76eb4b01fdafeedb00c64
Andrew Anca <andrewanca1998@hotmail.com>
2022-04-05 08:50:22 -0700

    page_info bookmark now working

--- pages/page_info_template/page_info_template.js
+++ pages/page_info_template/page_info_template.js
@@ -1,10 +1,12 @@
 //** function to display additional information when clicking the readmore button on the suggestion card */
 (() => {
+    const { fetchCurrentUserDocument, fetchDocuments, fetchDocument } = window.comidas.exports;
     const params = Object.fromEntries(window.location.search.substring(1).split('&').map(param => param.split('=')));
     // populate cards based on user type
     async function populateCardsDynamically(type) {
 
         const snapshot = await db.collection(type).where('id', '==', params.code).get();
+        const bookmarkRef = document.querySelector('.bookmark');
 
         const [doc] = snapshot.docs;
         // set vars for each piece of info we want to get from firestore db colelction
@@ -15,12 +17,17 @@
         const infoCardWebsite = doc.data().website;
         const infoCardPhone = doc.data().phone;
         // attach the selected info from firestore to html divs
+
+        displayBookmarkState(bookmarkRef, await fetchBookmarkState(params.code));
+
         document.querySelector('#picture').setAttribute("src", infoCardImage);
         document.querySelector('.card-title').innerHTML = infoCardName;
         document.querySelector('.card-address').innerHTML = infoCardAddress;
         document.querySelector('.card-hours').innerHTML = infoCardHours;
         document.querySelector('.card-website').innerHTML = infoCardWebsite;
         document.querySelector('.card-phone').innerHTML = infoCardPhone;
+
+        bookmarkRef.onclick = () => toggleBookmarkState(bookmarkRef, params.code);
     }
     //** function to check which "type" the user is */
     firebase.auth().onAuthStateChanged(user => {
@@ -41,31 +48,33 @@
             });
         }
     });
-    // code for the bookmark/save button
-    const saveBtnRef = document.querySelector('#saveIcon');
+    // function to check the state of the bookmark and if it should be filled or not
+    function displayBookmarkState(ref, shouldToggle) {
+        if (shouldToggle) {
+            ref.classList.remove('bi-bookmark');
+            ref.classList.add('bi-bookmark-fill');
+        } else {
+            ref.classList.remove('bi-bookmark-fill');
+            ref.classList.add('bi-bookmark');
+        }
+    }
 
-    saveBtnRef.setAttribute('data-id', params.code);
+    async function fetchBookmarkState(id) {
+        const snapshot = await fetchCurrentUserDocument();
 
-    const suggestionCode = saveBtnRef.getAttribute('data-id');
+        return snapshot.data().favourites.includes(id.toUpperCase());
+    }
 
-    saveBtnRef.onclick = () => {
-        if (!suggestionCode) return console.warn("This button does not contain a data-id code");
+    async function toggleBookmarkState(ref, id) {
+        const snapshot = await fetchCurrentUserDocument();
+        const currentState = await fetchBookmarkState(id);
 
-        firebase.auth().onAuthStateChanged(async(user) => {
-            if (user) { // This should only work if the user is signed in
-                const userDoc = db.collection("users").doc(user.uid);
-
-                try {
-                    // Add TYPE_ID to user's favourites
-                    const result = await userDoc.update({
-                        favourites: firebase.firestore.FieldValue.arrayUnion(suggestionCode)
-                    });
-                    // Success
-                } catch (e) {
-                    // Failure
-                    // Error stored in 'e'
-                }
-            }
-        });
+        if (!currentState) {
+            displayBookmarkState(ref, true);
+            await snapshot.ref.update({ favourites: firebase.firestore.FieldValue.arrayUnion(id.toUpperCase()) })
+        } else {
+            displayBookmarkState(ref, false)
+            await snapshot.ref.update({ favourites: firebase.firestore.FieldValue.arrayRemove(id.toUpperCase()) })
+        }
     }
 })();
